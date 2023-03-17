0864db2e0d7a1982c8059afc42d518aa089738e5
Sam Gleske <sam.mxracer@gmail.com>
2023-03-16 20:35:39 -0400

    Fix imports

--- source/Panel.cpp
+++ source/Panel.cpp
@@ -18,7 +18,6 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #include "Color.h"
 #include "Command.h"
 #include "Dialog.h"
-#include "text/Format.h"
 #include "FillShader.h"
 #include "text/Format.h"
 #include "GameData.h"
